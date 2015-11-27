ae77a89f47bb57d70935394ed956baf082970821
AkkiShashank <shashank.gupta1663@gmail.com>
2015-11-27 19:29:35 +0530

    first commit

---
+++ app/src/main/java/com/test/mandap/gcmreceiver/QuickstartPreferences.java
@@ -0,0 +1,10 @@
+package com.test.mandap.gcmreceiver;
+
+/**
+ * Created by Shashank Gupta on 13-Aug-15.
+ */
+public class QuickstartPreferences {
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}
